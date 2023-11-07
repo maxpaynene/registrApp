1a4570eacf3b721f764d58f5b75a8ba75d72def0
maxpaynene <uletron1992@hotmail.com>
2023-11-06 23:37:12 -0300

    second push

--- src/app/pages/main/home/home.page.ts
+++ src/app/pages/main/home/home.page.ts
@@ -1,21 +1,29 @@
-import { Component, OnInit, inject } from '@angular/core';
-import { FirebaseService } from 'src/app/services/firebase.service';
-import { UtilsService } from 'src/app/services/utils.service';
-import { AddUpdateStudentsComponent } from 'src/app/shared/components/add-update-students/add-update-students.component';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-import { BarcodeScanner } from '@capacitor-community/barcode-scanner';
-import * as JsBarcode from 'jsbarcode';
+import {
+  Component,
+  Inject,
+  Injectable,
+  Input,
+  OnInit,
+  inject,
+} from "@angular/core";
+import { FirebaseService } from "src/app/services/firebase.service";
+import { UtilsService } from "src/app/services/utils.service";
+import { AddUpdateStudentsComponent } from "src/app/shared/components/add-update-students/add-update-students.component";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"],
 })
 export class HomePage implements OnInit {
   firebaseSvc = inject(FirebaseService);
   utilsSvc = inject(UtilsService);
+  divPhoto: boolean = false;
+  html5QrcodeScanner: Html5QrcodeScanner;
+  @Input() hrefUrl: string = "";
 
-  //ngOnInit() {}
+  ngOnInit(): void {}
 
   signOut() {
     this.firebaseSvc.signOut();
@@ -24,94 +32,43 @@ export class HomePage implements OnInit {
   addUpdateStudnets() {
     this.utilsSvc.presentModal({
       component: AddUpdateStudentsComponent,
-      cssClass: 'add-update-modal',
+      cssClass: "add-update-modal",
     });
   }
 
-  async takeImage() {
-    const DataUrl = (await this.utilsSvc.takePicture('Escanea tu QR')).dataUrl;
-  }
-
-  // https://www.npmjs.com/package/angularx-qrcode
-  // https://www.npmjs.com/package/jsbarcode
-  qrCodeString = 'This is a secret qr code message';
-  barCodeString = '12345566765';
-  scannedResult: any;
-  // barScannedResult: any;
-  content_visibility = '';
-
-  constructor() {} // private barcodeScanner: BarcodeScanner
-
-  ngOnInit(): void {
-    JsBarcode('#barcode', this.barCodeString, {
-      // format: "pharmacode",
-      lineColor: '#0aa',
-      width: 4,
-      height: 200,
-      displayValue: false,
+  public async takeImage() {
+    this.setDivPhoto(true);
+    this.delay(1000).then(() => {
+      this.html5QrcodeScanner = new Html5QrcodeScanner(
+        "reader",
+        { fps: 30, qrbox: { width: 250, height: 250 } },
+        /* verbose= */ false
+      );
+      this.html5QrcodeScanner.render(this.onScanSuccess, this.onScanFailure);
     });
-  }
-
-  // startScan() {
-  //   this.barcodeScanner.scan().then(barcodeData => {
-  //     console.log('Barcode data', barcodeData);
-  //     this.scannedResult = barcodeData?.text;
-  //    }).catch(err => {
-  //        console.log('Error', err);
-  //    });
-  // }
-
-  async checkPermission() {
-    try {
-      // check or request permission
-      const status = await BarcodeScanner.checkPermission({ force: true });
-      if (status.granted) {
-        // the user granted permission
-        return true;
-      }
-      return false;
-    } catch (e) {
-      console.log(e);
-      return false;
-    }
-  }
-
-  async startScan() {
-    try {
-      const permission = await this.checkPermission();
-      if (!permission) {
-        return;
-      }
-      await BarcodeScanner.hideBackground();
-      document.querySelector('body').classList.add('scanner-active');
-      this.content_visibility = 'hidden';
-      const result = await BarcodeScanner.startScan();
-      console.log(result);
-      BarcodeScanner.showBackground();
-      document.querySelector('body').classList.remove('scanner-active');
-      this.content_visibility = '';
-      if (result?.hasContent) {
-        this.scannedResult = result.content;
-        console.log(this.scannedResult);
-      }
-    } catch (e) {
-      console.log(e);
-      this.stopScan();
-    }
-  }
-
-  stopScan() {
-    BarcodeScanner.showBackground();
-    BarcodeScanner.stopScan();
-    document.querySelector('body').classList.remove('scanner-active');
-    this.content_visibility = '';
-  }
-
-  ngOnDestroy(): void {
-    this.stopScan();
   }
 
   routerlinkIntro() {
-    this.utilsSvc.routerLink('/main/intro');
+    this.utilsSvc.routerLink("/main/intro");
+  }
+
+  onScanSuccess(decodedText, decodedResult) {
+    document.getElementById("html5-qrcode-button-camera-stop").click();
+    console.log(decodedText);
+    console.log(decodedResult);
+    this.hrefUrl = decodedText;
+    console.log(this.hrefUrl);
+    //console.log(document.getElementById("#hrefUrl"));
+    //document.getElementById("#hrefUrl").click();
+  }
+
+  onScanFailure(error) {
+    console.warn(`Code scan error = ${error}`);
+  }
+
+  private setDivPhoto = async (b: boolean) => (this.divPhoto = b);
+
+  delay(time) {
+    return new Promise((resolve) => setTimeout(resolve, time));
   }
 }
